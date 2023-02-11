1aafeff21168e372f438f2396481bf1d04fd80da
Michael <mr201190@gmail.com>
2023-02-11 14:36:32 +0100

    merge

--- config.def.h
+++ config.def.h
@@ -132,15 +132,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-<<<<<<< HEAD
 unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
-=======
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
->>>>>>> 64db48a (apply alpha patch)
 static unsigned int defaultrcs = 257;
 
 /*
