bf872f86508744a31ada3dfe9595336bf3040286
Michael <mr201190@gmail.com>
2023-02-11 15:37:55 +0100

    remove double declaration

--- config.def.h
+++ config.def.h
@@ -135,7 +135,6 @@ static const char *colorname[] = {
 unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
-static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
